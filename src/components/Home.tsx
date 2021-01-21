b143eda128b82bdd7403042836575879b5cfc079
Pacien Boisson <pakokrew@gmail.com>
2021-01-21 11:29:40 +0400

    clean & chakra

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Doser
+        </h1>
+       <main>
+         <p>
+           Coucou
+         </p>
+       </main>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
